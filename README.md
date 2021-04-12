d9dd43e483d334bb6b4c4ff89165a831b9dbf34d
Anon Karytski <heyheyjude23@gmail.com>
2021-04-12 21:43:23 +0300

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# rss-lang
-React.js app for study english words based on book 4000 essential english words
-
-
-
-Used technologies: JS, React.js, Redux (Redux thunk), SCSS, Node.js
--- src/pages/Games/PuzzleGame/components/Symbol.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import classesCss from '../PuzzleGame.module.scss'
-
-export default function Symbol({symbol}){
-	return(
-		<div className={classesCss.Symbol}><span>{symbol} </span></div>
-	)
-}
